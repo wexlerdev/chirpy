a2dff6bad1e27ff89412167d47ea148b76405fb3
wexlerdev <alexwexlerdev@gmail.com>
2025-06-04 18:50:08 -0500

    added validate chirp logic

---
+++ handler_validate.go
@@ -0,0 +1,53 @@
+package main
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+
+func validateChirpHandler(w http.ResponseWriter, req * http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		Body string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't Decode params", err)
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "chirp is too long", nil)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, returnVals {
+		Body: cleanString(params.Body),
+	})
+}
+
+func cleanString(str string) string {
+	profanity := []string{"kerfuffle", "sharbert", "fornax"}
+	const cleanWurd = "****"
+
+	strSlice := strings.Split(str, " ")
+
+	for _, badWurd := range profanity {
+		for i, bodyWurd := range strSlice {
+			if strings.ToLower(bodyWurd) == badWurd {
+				strSlice[i] = cleanWurd
+			}
+		}
+	}
+
+	finalString := strings.Join(strSlice, " ")
+
+	return finalString
+}
--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
+	"fmt"
 )
 
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+type chirpBody struct {
+	Body string
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -13,16 +23,52 @@ func main() {
 		Addr:		":8080",
 	}
 
+	apiConfig := apiConfig{}
+
+	mux.HandleFunc("GET /admin/healthz", readinessHandler)
+	mux.HandleFunc("GET /admin/metrics", apiConfig.getRequestCountsHandler)
+	mux.HandleFunc("POST /admin/reset", apiConfig.resetRequestCountsHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
+
 	fileServerHandler := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app/", fileServerHandler))
-	mux.HandleFunc("/", readinessHandler)
+	fileServerHandlerNoPrefix := http.StripPrefix("/app/", fileServerHandler)
+	mux.Handle("/app/", apiConfig.middlewareMetricsInc(fileServerHandlerNoPrefix))
 
 
 	server.ListenAndServe()
 }
 
+
 func readinessHandler(writer http.ResponseWriter, req *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) getRequestCountsHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(200)
+	htmlString :=`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+	outputString := fmt.Sprintf(htmlString, cfg.fileserverHits.Load())
+	w.Write([]byte(outputString))
+}
+
+func (cfg *apiConfig) resetRequestCountsHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte("Request Counts Reset"))
+}
+
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
